8bce20c62ac39f9806235b7649635901e040bf9d
Tyler Godfrey <tyler.r.godfrey@emcins.com>
2020-10-20 12:42:06 -0500

    fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,10 +4,10 @@ import rxjs/add/operator/map;
 
 @Injectable()
 export class ProductService {
-  private string _albumUrl = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
   
   getAlbum(id: number) {
-	  return this._http.get(_albumUrl);
+	  return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
