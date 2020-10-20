c4bb85ac3a8569428886e681bebab1776194b882
Tyler Godfrey <tyler.r.godfrey@emcins.com>
2020-10-20 15:49:30 -0500

    i will try this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,9 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
-const appRoutes: Routes =  [ { "path": "products", "component": ProductListComponent },
-						{ "path": "product/:id", "component": ProductPageComponent }, 
-						{ "path": '', "redirectTo": "products", "pathMatch": "full" }	];
+const appRoutes: Routes =  [ { path: "products", component: ProductListComponent },
+						{ path: "product/:id", component: ProductPageComponent }, 
+						{ path: '', redirectTo: "products", pathMatch: "full" }	];
 @NgModule({
   declarations: [
     AppComponent,
