527a90a03a61b839d13392af3f9fbc0bce7de724
Tyler Godfrey <tyler.r.godfrey@emcins.com>
2020-10-20 13:52:41 -0500

    fix

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductTracklistingComponent implements OnInit {
   albumInfo: Album;
-  constructor(_productService: ProductService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
 	  this._productService.getAlbum(1).subscribe( response => this.albumInfo = response );
