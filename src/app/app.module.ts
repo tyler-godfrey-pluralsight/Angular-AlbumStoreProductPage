42c3f20a1828aab1c4fd3def4f2b379082a37761
Tyler Godfrey <tyler.r.godfrey@emcins.com>
2020-10-20 15:42:06 -0500

    forgot bracket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
-const appRoutes: Routes =  [ "path": "products", "component": "ProductListComponent" },
+const appRoutes: Routes =  [ { "path": "products", "component": "ProductListComponent" },
 						{ "path": "product/:id", "component": "ProductPageComponent" }, 
 						{ "path": '', "redirectTo": "products", "pathMatch": "full" }	];
 @NgModule({
